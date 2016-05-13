19f03f430e1bc9f04c44c021254c2d649d21d18d
Andrew Jones <andrew@jones.ec>
2016-05-13 17:40:41 -0400

    Added a primitive resource for fair scheduler queues

--- cookbooks/bcpc-hadoop/recipes/yarn_schedulers.rb
+++ cookbooks/bcpc-hadoop/recipes/yarn_schedulers.rb
@@ -23,3 +23,35 @@ template "/etc/hadoop/conf/capacity-scheduler.xml" do
   variables(:options =>
             node[:bcpc][:hadoop][:yarn][:scheduler][:capacity][:xml])
 end
+
+template '/etc/hadoop/conf/fair-scheduler.fresh.xml' do
+  source 'yarn/fair-scheduler.xml.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  variables(lazy{{
+              :queues =>
+                run_context.resource_collection.select{ |r| r.class ==
+                  Chef::Resource::BcpcHadoopFairSchedulerQueue }
+                 }})
+end
+
+yarn = node[:bcpc][:hadoop][:yarn]
+minimum_cores = yarn['scheduler']['fair']['min-vcores']
+minimum_ram = yarn['scheduler']['minimum-allocation-mb'] * minimum_cores
+
+bcpc_hadoop_fair_scheduler_queue 'default' do
+  weight 1.0
+  fair_share_preemption_timeout yarn['fairsharepreemptiontimeout']
+  min_resources "#{minimum_ram} mb,#{minimum_cores}vcores"
+  type 'parent'
+end
+
+bcpc_hadoop_fair_scheduler_queue 'data_license' do
+  scheduling_policy 'fifo'
+  acl_administer_apps = 'odsuser,dluser'
+  weight 2.0
+  max_running_apps 3
+  min_resources '5000 mb,5vcores'
+  max_resources '256000 mb,200vcores'
+end
