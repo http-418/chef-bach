8e9ac9e2dbaf5b95cb0b5d32407fb93fd57d05da
Andrew Jones <andrew@jones.ec>
2016-05-13 17:41:53 -0400

    Remove unused scheduler templates

--- cookbooks/bcpc-hadoop/recipes/yarn_schedulers.rb
+++ cookbooks/bcpc-hadoop/recipes/yarn_schedulers.rb
@@ -1,22 +1,3 @@
-hadoop_templates =
-  %w{
-   fair-scheduler.xml
-  }
-
-hadoop_templates.each do |t|
-   template "/etc/hadoop/conf/#{t}" do
-     source "hdp_#{t}.erb"
-     mode 0644
-     variables(:nn_hosts => node[:bcpc][:hadoop][:nn_hosts],
-               :zk_hosts => node[:bcpc][:hadoop][:zookeeper][:servers],
-               :jn_hosts => node[:bcpc][:hadoop][:jn_hosts],
-               :rm_hosts => node[:bcpc][:hadoop][:rm_hosts],
-               :dn_hosts => node[:bcpc][:hadoop][:dn_hosts],
-               :hs_hosts => node[:bcpc][:hadoop][:hs_hosts],
-               :mounts => node[:bcpc][:hadoop][:mounts])
-   end
-end
-
 template "/etc/hadoop/conf/capacity-scheduler.xml" do
   source "generic_site.xml.erb"
   mode 0644
@@ -24,7 +5,7 @@ template "/etc/hadoop/conf/capacity-scheduler.xml" do
             node[:bcpc][:hadoop][:yarn][:scheduler][:capacity][:xml])
 end
 
-template '/etc/hadoop/conf/fair-scheduler.fresh.xml' do
+template '/etc/hadoop/conf/fair-scheduler.xml' do
   source 'yarn/fair-scheduler.xml.erb'
   owner 'root'
   group 'root'
@@ -45,13 +26,4 @@ bcpc_hadoop_fair_scheduler_queue 'default' do
   fair_share_preemption_timeout yarn['fairsharepreemptiontimeout']
   min_resources "#{minimum_ram} mb,#{minimum_cores}vcores"
   type 'parent'
-end
-
-bcpc_hadoop_fair_scheduler_queue 'data_license' do
-  scheduling_policy 'fifo'
-  acl_administer_apps = 'odsuser,dluser'
-  weight 2.0
-  max_running_apps 3
-  min_resources '5000 mb,5vcores'
-  max_resources '256000 mb,200vcores'
 end
