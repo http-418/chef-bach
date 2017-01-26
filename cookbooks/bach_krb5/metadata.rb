f3a0e6dc7c39bc09af800350de6ca5a71abcf05e
Clay Baenziger <cbaenziger@bloomberg.net>
2017-01-26 13:13:37 -0500

    Need to include krb5 to depend on its recipes

--- cookbooks/bach_krb5/metadata.rb
+++ cookbooks/bach_krb5/metadata.rb
@@ -6,7 +6,7 @@ description      'Wrapper cookbook for krb5 community cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'krb5', '~> 2.0.0'
+depends 'krb5'
 depends 'bcpc', '= 0.1.0'
 depends 'bcpc-hadoop', '= 0.1.0'
 
